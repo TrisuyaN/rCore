55dc74001a895b6a54b473046a466c82476c20f1
al_1suyan <trisuyan@gmail.com>
2024-05-31 09:12:21 +0000

    Pending changes exported from your codespace

--- os/src/task/context.rs
+++ os/src/task/context.rs
@@ -6,3 +6,28 @@ pub struct TaskContext {
     sp: usize,
     s: [usize; 12],
 }
+
+impl TaskContext {
+    /// init task context
+    pub fn zero_init() -> Self {
+        Self {
+            ra: 0,
+            sp: 0,
+            s: [0; 12],
+        }
+    }
+
+    /// set task context {__restore ASM funciton, kernel stack, s_0..12 }
+    /// Now we has multiple apps (tasks) and each app (task) has it's own kernel stack. 
+    /// so we need to give the specific kstack_ptr.
+    pub fn goto_restore(kstack_ptr: usize) -> Self {
+        extern "C" {
+            fn __restore();
+        }
+        Self {
+            ra: __restore as usize,
+            sp: kstack_ptr,
+            s: [0; 12],
+        }
+    }
+}
